656719aadc47deaa9cdc63ac81b7cbdc38767110
Andrei Bezmen <andryshabez26@gmail.com>
2021-02-25 00:46:40 +0300

    feat: replace CORS Anywhere server

--- src/services/data.js
+++ src/services/data.js
@@ -1,6 +1,6 @@
 export default async function getData() {
   const response = await fetch(
-    'https://evening-basin-27448.herokuapp.com/http://www.mrsoft.by/data.json',
+    'https://secret-everglades-21695.herokuapp.com/http://www.mrsoft.by/data.json',
   );
   return await response.json();
 }
